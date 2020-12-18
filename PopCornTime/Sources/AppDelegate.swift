d5c6089965ae89d4d13a63a845396e60f2637551
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2020-12-18 12:52:45 +0400

    firs Notofocation

--- PopCornTime/Sources/AppDelegate.swift
+++ PopCornTime/Sources/AppDelegate.swift
@@ -6,29 +6,96 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     // MARK: Stored properties
-
+    
     private lazy var mainWindow = UIWindow()
     private let router = AppCoordinator().strongRouter
-
+    
     // MARK: UIApplicationDelegate
-
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         configureUI()
         router.setRoot(for: mainWindow)
+        registerForPushNotifications()
+        
+        
+        // Check if launched from notification
+        let notificationOption = launchOptions?[.remoteNotification]
+        
+        // 1
+        if  let notification = notificationOption as? [String: AnyObject],
+            let aps = notification["aps"] as? [String: AnyObject] {
+            // 2
+            // NewsItem.makeNewsItem(aps)
+            
+            // 3
+            (mainWindow.rootViewController as? UITabBarController)?.selectedIndex = 1
+        }
         return true
+        
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+      fetchCompletionHandler completionHandler:
+      @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+      guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+        completionHandler(.failed)
+        return
+      }
+      //NewsItem.makeNewsItem(aps)
     }
 
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        print("Failed to register: \(error)")
+    }
+    
     // MARK: Helpers
-
+    
     private func configureUI() {
         UIView.appearance().overrideUserInterfaceStyle = .light
     }
-
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(
+                options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+            }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
 }
 
